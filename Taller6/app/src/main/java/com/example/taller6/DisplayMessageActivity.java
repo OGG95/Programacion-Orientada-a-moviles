eca77ee3195cec3cedf66f443f1c041ef066d4b1
ogg95 <omargaviria21@gmail.com>
2022-10-06 16:18:47 -0500

    conexion app - backend

--- Taller6/app/src/main/java/com/example/taller6/DisplayMessageActivity.java
+++ Taller6/app/src/main/java/com/example/taller6/DisplayMessageActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
 public class DisplayMessageActivity extends AppCompatActivity {
@@ -15,7 +16,6 @@ public class DisplayMessageActivity extends AppCompatActivity {
         // Get the Intent that started this activity and extract the string
         Intent intent = getIntent();
         String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
-
         // Capture the layout's TextView and set the string as its text
         TextView textView = findViewById(R.id.textView3);
         textView.setText(message);
--- Taller6/app/src/main/java/com/example/taller6/MainActivity.java
+++ Taller6/app/src/main/java/com/example/taller6/MainActivity.java
@@ -1,13 +1,25 @@
+
 package com.example.taller6;
-
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.DownloadManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.content.Intent;
 import android.widget.EditText;
 
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+
 public class MainActivity extends AppCompatActivity {
+    String message ="", enviar = "";
+    Intent intent;
     public static final String EXTRA_MESSAGE = "com.example.taller6.MESSAGE";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,11 +29,36 @@ public class MainActivity extends AppCompatActivity {
 
     /** Called when the user taps the Send button */
     public void sendMessage(View view) {
-        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        intent = new Intent(this, DisplayMessageActivity.class);
         EditText editText = (EditText) findViewById(R.id.editTextTextPersonName);
-        String message = editText.getText().toString();
-        intent.putExtra(EXTRA_MESSAGE, message);
-        startActivity(intent);
+        message = editText.getText().toString();
+        leerWS();
+
+    }
+
+    private void leerWS(){
+        String result ;
+        String ws = "http://10.0.2.2:8080/consultar/"+message;
+        StringRequest post = new StringRequest(Request.Method.GET, ws, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                try {
+                    JSONObject jsonObject = new JSONObject(response);
+                    enviar= jsonObject.getString("user");
+                    intent.putExtra(EXTRA_MESSAGE, enviar);
+                    startActivity(intent);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e("error",error.getMessage());
+            }
+        });
+        Volley.newRequestQueue(this).add(post);
     }
 }
 
